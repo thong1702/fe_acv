a8fe93cc4910112a695ec942b2705553dcbbc405
ThongTv <thongth@tecapro.com.vn>
2026-07-21 13:55:49 +0700

    add docker-compose for VPS deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiHost: 'http://180.93.42.16:8080',
-  authUrl: 'http://180.93.42.16:8080/auth',
-  apiUrl: 'http://180.93.42.16:8080/api'
+  apiHost: '',
+  authUrl: '/auth',
+  apiUrl: '/api'
 };
